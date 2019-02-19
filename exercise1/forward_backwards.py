d20e7961c15db41f91bf832ff93d8b7b3dd6cb6b
Carl Mueller <cmuell89@gmail.com>
2019-02-18 22:22:53 -0700

    Non-matrix based forward algorithm

--- exercise1/forward_backwards.py
+++ exercise1/forward_backwards.py
@@ -1,48 +1,45 @@
 import numpy as np
 import os
+from observations import y_obs_long, y_obs_short
+from parameters import pxk_xkm1, pyk_xk, px0
 
 
-def transtion_p(xk, x_km1):
-    pxk_xkm1 = [[0.020, 0.019, 0.000, 0.666],
-                [0.000, 0.025, 0.517, 0.000],
-                [0.163, 0.769, 0.466, 0.000],
-                [0.817, 0.187, 0.017, 0.334]]
+class ForwardBackwardsHMM():
 
-    return pxk_xkm1[xk, x_km1]
+    def __init__(self, transition_probs, evidence_probs, initial_probs, observations):
+        self.n_states = len(initial_probs)
+        self.init_probs = initial_probs
+        self.ev_probs = evidence_probs
+        self.trans_probs = transition_probs
+        self.n_observations = len(observations)
+        self.observations = observations
 
+    def foward_backwards(self):
+        pass
 
-def evidence_p(yk, xk):
-    pyk_xk = [[0.0338, 0.0000, 0.0000, 0.3273]
-              [0.0934, 0.0000, 0.0000, 0.0949]
-              [0.1356, 0.0000, 0.0000, 0.0311]
-              [0.1031, 0.0000, 0.0000, 0.0125]
-              [0.1350, 0.0000, 0.0000, 0.0113]
-              [0.0289, 0.0000, 0.0000, 0.3354]
-              [0.0968, 0.0000, 0.0000, 0.1094]
-              [0.1409, 0.0000, 0.0000, 0.0488]
-              [0.1117, 0.0000, 0.0000, 0.0149]
-              [0.1208, 0.0000, 0.0000, 0.0144]
-              [0.0000, 0.0842, 0.7353, 0.0000]
-              [0.0000, 0.2048, 0.1869, 0.0000]
-              [0.0000, 0.3774, 0.0195, 0.0000]
-              [0.0000, 0.3336, 0.0267, 0.0000]
-              [0.0000, 0.0000, 0.0316, 0.0000]]
+    def _forwards(self):
+        alphas = np.zeros((self.n_states, self.n_observations))
 
-    return pyk_xk[yk, xk]
+        # Initialization of f
+        obs_idx = self.observations[0]
+        for s in range(0, self.n_states):
+            alphas[s, 0] = self.init_probs[s] * self.ev_probs[obs_idx, s]
 
+        # alpha steps step
+        # we ignore time step 1
+        for t in range(1, self.n_observations):
+            # get the index value given observation at time t.
+            obs_idx = self.observations[t]
+            for s in range(0, self.n_states):
+                alphas[s, t] = self.ev_probs[obs_idx, s] * sum([alphas[s_plus_1, t - 1] * self.trans_probs[s_plus_1, s] for s_plus_1 in range(0, self.n_states)])
 
-def initial_p(xk):
-    px0 = [0, 0, 1, 0]
-    return px0[xk]
+        return alphas
 
+    def _backwards(self):
+        pass
 
-def import_nominal_parameters(filename):
-    curr_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(curr_path, filename)
-    with open(path) as f:
-        data = f.readlines()
-
-    for line in data:
-        print(line)
 
 if __name__ == "__main__":
+    print(pyk_xk.shape)
+    fbhmm = ForwardBackwardsHMM(pxk_xkm1, pyk_xk, px0, y_obs_short)
+    print(fbhmm._forwards())
