ae4c79734d4d64e67c2fe1abbde8a42f3fbd2633
Carl Mueller <cmuell89@gmail.com>
2019-02-26 00:20:56 -0700

    viterbi path name

--- exercise1/viterbi.py
+++ exercise1/viterbi.py
@@ -16,11 +16,11 @@ class ViterbiHMM():
         self.n_obs = len(observations)
         self.obs = observations
 
-    def likeliest_path(self):
+    def viterbi_path(self):
 
         init = np.array([eln(val) for val in np.nditer(self.init)])
         trans = np.array([[eln(v) for v in np.nditer(axis)] for axis in np.nditer(self.trans)]).reshape(self.n_states, self.n_states)
-        emis = np.array([[eln(v) for v in np.nditer(axis)] for axis in np.nditer(self.emis, order='F')]).reshape(self.n_states, self.n_obs)
+        emis = np.array([[eln(v) for v in np.nditer(axis)] for axis in np.nditer(self.emis)]).reshape(self.n_states, self.n_obs)
 
         best_states = []
 
@@ -30,14 +30,14 @@ class ViterbiHMM():
             for i in range(self.n_states):
                 for j in range(self.n_states):
                     trans_p[i, j] = elnsum(logprob[i], elnproduct(trans[i, j], emis[i, self.obs[k]]))
-            # Get the indices of the max probs, which give the best prior states.
+            # Get the indices of the max probs that give the best prior states.
             best_states.append(np.argmax(trans_p, axis=0))
             logprob = np.max(trans_p, axis=0)
 
         # Most likely final state.
         final_state = np.argmax(logprob)
         print(final_state)
-        # Reconstruct path by following links and then reversing.
+        # Reconstruct path by backtracking through likeliest states.
         prior_state = final_state
         best_path = [prior_state + 1]
         for best in reversed(best_states):
@@ -47,5 +47,5 @@ class ViterbiHMM():
 
 if __name__ == "__main__":
     vhmm = ViterbiHMM(pxk_xkm1, pyk_xk, px0, y_obs_short)
-    path, log_probs = vhmm.likeliest_path()
+    path, log_probs = vhmm.viterbi_path()
     print(path)
